4a6ccffe81407b663cd49fb2ddd6b56874020e39
HernandezM-dev <HernandezM.dev@gmail.com>
2020-09-21 10:49:18 -0400

    i dependencies and created other files and code for base api/db

---
+++ auth/authenticate-middleware.js
@@ -0,0 +1,47 @@
+/* 
+  complete the middleware code to check if the user is logged in
+  before granting access to the next middleware/route handler
+*/
+const jwt = require('jsonwebtoken')
+
+
+// confirms whether the call to the api comes with valid token
+const authenticate = (req, res, next) => {
+  const token = req.headers.authorization;
+  const secret = process.env.JWT_SECRET || "med-cab"
+
+  if(token){
+    jwt.verify(token,secret,(err, decodedToken) =>{
+      err ?
+      res.status(401).json({message: 'Token Not Authorized'})
+      : req.jwt = decodedToken
+    })
+  }else{
+    res.status(401).json({ message: 'Missing Token'});
+  }
+};
+
+//checks to see if user object matches table user data structure
+function isValid(user) {
+  return Boolean(user.email && user.password && typeof user.password === "string");
+}
+
+//creates token based off of user information
+function makeJwt({ id, username}) {
+  const payload = {
+      email,
+      subject: id,
+  };
+  const config = {
+      jwtSecret: process.env.JWT_SECRET || "is it secret, is it safe?",
+  };
+  const options = {
+      expiresIn: "8 hours",
+  };
+  return jwt.sign(payload, config.jwtSecret, options)
+}
+module.exports = {
+  authenticate,
+  isValid,
+  makeJwt,
+}
---
+++ data/migrations/20200921035347_med-cab.js
@@ -0,0 +1,34 @@
+const { nextTick } = require("process");
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('users', tbl =>{
+      tbl.increment('id').onDelete('RESTRICT').onUpate('RESTRICT');
+      tbl.string('email').notNullable().unique();
+      tbl.string('firstname').notNullable();
+      tbl.string('lastname').notNullable();
+      tbl.integer('phone').notNullable().unique();
+  })
+  .createTable('userInput', tbl =>{
+      tbl.increment('id');
+      tbl.integer('userid').references('roles.id').onDelete('RESTRICT').onUpate('RESTRICT');
+      tbl.boolean('input1').defaultTo(0);
+      tbl.boolean('input2').defaultTo(0);
+      tbl.boolean('input3').defaultTo(0);
+      tbl.boolean('input4').defaultTo(0);
+      tbl.boolean('input5').defaultTo(0);
+      tbl.boolean('input6').defaultTo(0);
+  })
+  .createTable('recommendations', tbl =>{
+      tbl.increment('id');
+      tbl.integer('userid').references('roles.id').onDelete('RESTRICT').onUpate('RESTRICT');
+      tbl.string('strain').notNullable()
+      tbl.string('location')
+      tbl.string('website')
+  })
+
+};
+
+exports.down = function(knex) {
+  
+};
