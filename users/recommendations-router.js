a6d42e83a0d0d421c53fddcd878e9cfd055cb52e
HernandezM-dev <HernandezM.dev@gmail.com>
2020-09-22 03:37:48 -0400

    completed recommendation endpoints and model

--- users/recommendations-router.js
+++ users/recommendations-router.js
@@ -15,7 +15,6 @@ router.get('/', (req, res) =>{
 
 router.get("/:userid", (req, res) => {
     const userId = req.params
-    console.log(userId)
     db.findBy(userId)
       .then((user) => {
         if (user) {
@@ -29,5 +28,53 @@ router.get("/:userid", (req, res) => {
       });
   });
 
+router.put('/:id', (req, res) =>{
+    const id = req.params.id
+    const userId = req.body.userid
+    const updates = req.body;
+  
+    db.findById(id)
+      .then(user=> {
+        user ?
+        db.update(id, updates)
+        .then(user => {
+            res.status(200).json({message: 'Recommendation Updated', user: user});
+          })
+          .catch(err => {
+            res.status(500).json({message: "Unable to update Recommendation", error: err});
+          })
+        : res.status(404).json({ message: "Unable to find Recommendation" });
+    })
+})
 
+router.post('/', (req, res) =>{
+    const set = req.body;
+    db.add(set)
+    .then(set =>{
+        res.status(200).json({message: 'Added Recommendation', rec: set})
+    })
+    .catch(err =>{
+        res.status(500).json({message: 'Unable to add recommendation', error: err})
+    })
+})
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+  
+    db.findById(id)
+      .then(set=> {
+        set ?
+        db.remove(id)
+            .then(remRes =>{
+                res.status(202).json({message: 'Recommendation Deleted'})
+            })
+            .catch(err =>{
+                res.status(500).json({message: 'Recommendation could not be deleted', error: err})
+            })
+        : res.status(404).json({message: 'Recommendation not found'})
+      })
+      .catch((error) => {
+        res.status(500).json({error: error.message});
+      });
+  });
 module.exports = router
